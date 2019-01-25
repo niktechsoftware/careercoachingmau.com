ddf36ce99ad4866a8425b77895e9dbe45f7a0b68
Amit <45931218+imamityadav@users.noreply.github.com>
2019-01-26 00:46:26 +0530

    gsgfs

--- application/config/database.php
+++ application/config/database.php
@@ -48,11 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'career';
+$db['default']['username'] = 'gfincpnt_career';
+$db['default']['password'] = 'rahul!123singh';
+$db['default']['database'] = 'gfincpnt_career';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
